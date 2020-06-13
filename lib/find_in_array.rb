79819bd15367ccb8823628f23f04f144a93cb1df
Hailey Ringier <haileyringier@outlook.com>
2020-06-13 00:02:57 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   counter = 0 
   while counter < array.length do 
-    array[counter].include? (value_to_find) 
+    array[counter] == value_to_find 
     return array[counter]
     counter += 1
   end
